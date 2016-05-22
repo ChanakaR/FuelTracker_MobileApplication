f31b0c0ebce5ec72a5d7c62bb04d7f2e3b293985
ChanakaR <bmcrathnayaka@gmail.com>
2016-05-22 06:53:17 +0530

    change the UI of home activity, and add classes TRIP and VEHICLE and FILLUP

--- app/src/main/java/com/example/bmcsoft/fueltracker/MainActivity.java
+++ app/src/main/java/com/example/bmcsoft/fueltracker/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/bmcsoft/fueltracker/objects/Trip.java
@@ -0,0 +1,36 @@
+package com.example.bmcsoft.fueltracker.objects;
+
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+/**
+ * Created by bmCSoft on 2016-05-21.
+ */
+public class Trip {
+    private Date date;
+    private String time;
+    private Vehicle vehicle;
+    private String description;
+    private boolean isStarted = false;
+    private ArrayList<FillUp> fill_up;
+
+    public void startTrip(Vehicle vehicle,String description){
+        this.fill_up = new ArrayList<>();
+        this.isStarted = true;
+        this.vehicle = vehicle;
+        this.description = description;
+        //get date from the system
+    }
+
+    public void endTrip(){
+        if(isStarted){
+            isStarted = false;
+        }
+    }
+
+    public void addFillUp(float amount,float unit_price,float total_cost,String fuel_type,String time){
+        FillUp fup = new FillUp(this.vehicle,amount,unit_price,total_cost,fuel_type,time);
+        fill_up.add(fup);
+    }
+}
